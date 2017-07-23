b5bf0ccf67ba9cd1937d160a9ec18e654ff8bee8
junaid <junaid1460@gmail.com>
2017-07-23 18:30:44 +0530

    models update

--- database/models.py
+++ database/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    _usn = models.CharField(max_length = 40)
+    _fname = models.CharField(max_length = 100)
+
+class Faculty(models.Model):
+    _name = models.CharField(max_length = 100)
+
+class Document(models.Model):
+    _document_of = models.ForeignKey(Student)
+    _file = models.CharField(max_length = 500)
+
+class VerifiedDoc(models.Model):
+    _verified_by = models.ForeignKey(Faculty)
+    _document = models.OneToOneField(Document)
+
