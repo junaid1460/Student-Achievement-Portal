fdc71da7a794c1c9ec29abcab2d17bf013339103
junaid <junaid1460@gmail.com>
2017-08-08 00:15:33 +0530

    nothing

--- database/models.py
+++ database/models.py
@@ -7,6 +7,7 @@ class Extended(models.Model):
     type = models.CharField(max_length=100)
 
 class Document(models.Model):
+    _date = models.DateTimeField() #year at which achievement earned
     _user = models.ForeignKey(Extended) #belongs to which user?
     _file = models.FileField() # document
     _title = models.CharField(max_length = 1000, default = '') #title of achievement
@@ -14,7 +15,7 @@ class Document(models.Model):
     _place = models.CharField(max_length = 400, default="") #place if needed
     _created_at = models.DateTimeField(auto_now_add=True) #automatic created date field
     _verified = models.BooleanField(default= False) # whether verified or not
-    _date = models.DateField() #year at which achievement earned
+
     _domain = models.CharField(max_length = 20, default = "") #national, international, local etc.
     _category = models.CharField(max_length = 20, default = "") #academic, cultural or sport
     _avail_for_verification = models.BooleanField(default= False) #whether sent for verification or not
