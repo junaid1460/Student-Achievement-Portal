5baf5e9f285d016552dec6000c1b916b6c3f6e50
junaid <junaid1460@gmail.com>
2017-08-19 11:23:46 +0530

    Added Sub category option as per new requirement. Sub categories are introduced for Technical and Academic Category now.

--- database/models.py
+++ database/models.py
@@ -13,17 +13,20 @@ class Document(models.Model):
     _user = models.ForeignKey(Extended) #belongs to which user?
     _file = models.FileField() # document
     _title = models.CharField(max_length = 1000, default = '') #title of achievement
-    _year = models.IntegerField(default=0) #academic year
+    _year = models.IntegerField(default=0) #academic year ()
     _place = models.CharField(max_length = 400, default="") #place if needed
     _created_at = models.DateTimeField(auto_now_add=True) #automatic created date field
     _verified = models.BooleanField(default= False) # whether verified or not
     _domain = models.CharField(max_length = 20, default = "") #national, international, local etc.
     _category = models.CharField(max_length = 20, default = "") #academic, cultural or sport
-    _avail_for_verification = models.BooleanField(default= False) #whether sent for verification or not
+    _sub_cat = models.CharField(max_length = 50, default = None) #sub category
+    # _avail_for_verification = models.BooleanField(default= False) #whether sent for verification or not
     _has_error = models.BooleanField(default = False)
     _error_message = models.CharField(max_length = 2000, default="")
     def __str__(self):
         return self._title
+
+
 
 
 class VerifiedDoc(models.Model):
