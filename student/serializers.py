7d283220e81e7ecd2fb1a79e85e17b3b95887e8b
junaid <junaid1460@gmail.com>
2017-08-20 11:57:56 +0530

    Modified serializers to return field names.

--- student/serializers.py
+++ student/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from database.models import Document
 class DocumentSerializer(serializers.ModelSerializer):
+    _domain = serializers.SerializerMethodField()
+    _category = serializers.SerializerMethodField()
+    _year = serializers.SerializerMethodField()
+    _type = serializers.SerializerMethodField()
+    _sub_cat = serializers.SerializerMethodField()
+
     class Meta:
         model = Document
-        fields = ('id', '_file','_domain','_category',
+        fields = ('id', '_file','_domain','_category', '_type',
         '_event_time','_place', '_title','_year','_verified',
-        '_avail_for_verification', '_created_at', '_has_error', '_error_message')
+        '_sub_cat', '_created_at', '_has_error', '_error_message')
+    def get__domain(self, obj):
+        return obj.get__domain_display()
+    def get__category(self, obj):
+        return obj.get__category_display()
+    def get__year(self, obj):
+        return obj.get__year_display()
+    def get__type(self, obj):
+        return obj.get__type_display()
+    def get__sub_cat(self, obj):
+        return obj.get__sub_cat_display()
