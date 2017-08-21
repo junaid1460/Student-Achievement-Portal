bc95d612532588d89415a57505d54ff143b5400a
junaid <junaid1460@gmail.com>
2017-08-22 01:10:31 +0530

    created DocuementSetSerializer clas since adding getters do cause problem in POST operations

--- student/serializers.py
+++ student/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
 from database.models import Document
+
+class DocumentSetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = ('id', '_file','_domain','_category', '_type',
+        '_event_time','_place', '_title','_year','_verified',
+        '_sub_cat', '_created_at', '_has_error', '_error_message')
+
+
+
 class DocumentSerializer(serializers.ModelSerializer):
     _domain = serializers.SerializerMethodField()
     _category = serializers.SerializerMethodField()
