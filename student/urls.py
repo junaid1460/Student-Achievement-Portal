70a2f8e6b3c5c48f4f6f1a07510943fbc02121d1
junaid <junaid1460@gmail.com>
2017-08-22 01:11:08 +0530

    Since getters do cause problem in POST new route added docs/upload

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import DocumentListView,delete
+from .views import DocumentListView,delete, DocumentSetView
 urlpatterns = [
   url(r'^docs$', DocumentListView.as_view()),
+  url(r'^docs/upload$', DocumentSetView.as_view()),
   url(r'^docs/delete$', delete)
 ]
