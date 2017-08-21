40b097262384cdbfc9d47a19cf5e1e8c14f1a9cb
junaid <junaid1460@gmail.com>
2017-08-22 01:06:53 +0530

    added one more route for getting list of students for the search parameters.

--- stats/urls.py
+++ stats/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import stats_view, StatsListing
+from .views import stats_view, StatsListing, StudentStatsListing
 urlpatterns = [
   url(r'^$', stats_view),
   url(r'^search$', StatsListing.as_view()),
+  url(r'^searchstudent$', StudentStatsListing.as_view()),
 ]
