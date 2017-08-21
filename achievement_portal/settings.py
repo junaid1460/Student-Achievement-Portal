69f9e3aa4ad6ce8ccf68377d175b8186de4e497b
junaid <junaid1460@gmail.com>
2017-08-22 01:03:55 +0530

    changed database from sqlite to mysql

--- achievement_portal/settings.py
+++ achievement_portal/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'achievement_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'achievement_portal',
+        'USER': 'root',
+        'PASSWORD': 'crook@#$',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
