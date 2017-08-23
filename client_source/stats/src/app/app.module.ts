06d9bbbea6e5964d4aa30d954944f32c21393bd4
junaid <junaid1460@gmail.com>
2017-08-23 16:18:12 +0530

    added two new component to the registry

--- client_source/stats/src/app/app.module.ts
+++ client_source/stats/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {MaterialModule} from '@angular/material'
 import { AppComponent } from './app.component';
@@ -7,9 +8,16 @@ import {AppService} from './app.service'
 import {HttpModule} from '@angular/http'
 import {FormsModule} from '@angular/forms'
 import 'hammerjs'
+import {RouterModule} from '@angular/router'
+import {StudentComponent} from './student.component'
+import {StudentSearchComponent} from './student.component.search'
+import {routes, RouterComponent} from './app.routes'
 @NgModule({
   declarations: [
-    AppComponent
+    RouterComponent,
+    AppComponent,
+    StudentComponent,
+    StudentSearchComponent
   ],
 
   imports: [
@@ -17,9 +25,10 @@ import 'hammerjs'
     BrowserAnimationsModule,
     MaterialModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [ AppService ],
-  bootstrap: [AppComponent]
+  providers: [ AppService , {provide :APP_BASE_HREF,useValue: '/stats'}],
+  bootstrap: [RouterComponent]
 })
 export class AppModule { }
