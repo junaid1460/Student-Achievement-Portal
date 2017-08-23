dddf9e7fb1691a31b7bac7a55354fe954fe0e697
junaid <junaid1460@gmail.com>
2017-08-23 16:14:49 +0530

    Added route to get student details since the stats view requires it.

--- stats/urls.py
+++ stats/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from .views import stats_view, StatsListing, StudentStatsListing
+from .views import stats_view, StatsListing, StudentStatsListing, StudentView, CurrentStudentList
 urlpatterns = [
   url(r'^$', stats_view),
   url(r'^search$', StatsListing.as_view()),
   url(r'^searchstudent$', StudentStatsListing.as_view()),
+  url(r'^getstudent$', StudentView.as_view()),
+  url(r'^currentstudent$', CurrentStudentList.as_view()),
+
 ]
