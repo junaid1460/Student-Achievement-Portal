590d01c863bbb3aa2729143bd1906a4fc6327948
junaid <junaid1460@gmail.com>
2017-08-24 16:07:36 +0530

    Modified service to supprt sorting params

--- client_source/stats/src/app/app.service.ts
+++ client_source/stats/src/app/app.service.ts
@@ -4,25 +4,26 @@ import {Http, URLSearchParams, Headers} from '@angular/http'
 @Injectable()
 export class AppService {
     docs = []
-  nextpage = null
-  count = 0;
-  students = []
-  snextpage = null
-  scount = 0
-  domain_query = new Set()
-  sub_cat_query = {};
-  tmp_squery = {}
-  cat_query = new Set()
+    nextpage = null
+    count = 0;
+    students = []
+    snextpage = null
+    scount = 0
+    domain_query = new Set()
+    sub_cat_query = {};
+    tmp_squery = {}
+    cat_query = new Set()
     year_query = new Set()
-  usns = new Set()
-  years = new Set()
-  cdoc_id = null;
-  navinfo = null;
-  type_query = new Set()
-  myresult:number = 0
+    usns = new Set()
+    years = new Set()
+    cdoc_id = null;
+    navinfo = null;
+    type_query = new Set()
+    myresult:number = 0
     old_header:Headers;
+    sortby:string;
     constructor(private _http:Http){}
-    getDocs(keys, domains, cats, years, type, subcat,student = false ){
+    getDocs(keys, domains, cats, years, type, subcat, sortby, student = false ){
         var  link = '/api/stats/search?format=json';
         if(student == true){
             link = '/api/stats/searchstudent?format=json';
@@ -30,13 +31,14 @@ export class AppService {
         var tmp = new URLSearchParams()
         let headers = new Headers({
             'Content-Type': 'application/json',
-            'data':JSON.stringify({
-            'keys':keys,
-            'domains':domains,
-            'cats':cats,
-            'years':years,
-            'types':type,
-            'subcat':subcat
+            'data' : JSON.stringify({
+            'keys' : keys,
+            'domains' : domains,
+            'cats' : cats,
+            'years' : years,
+            'types' : type,
+            'subcat' : subcat,
+            'sortby' : sortby?sortby:""
             })
 
         })
@@ -53,6 +55,7 @@ export class AppService {
         return this._http.get(link,{ headers:this.old_header})
     }
     getDataWithFilter(header:Headers){
+        console.log(header)
         return this._http.get('/api/stats/search?format=json',{headers:  header})
     }
     getCurrentStudentData(username){
