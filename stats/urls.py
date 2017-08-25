25fb6ffc0b43bac18369e934ebec3d12f1ad4171
junaid <junaid1460@gmail.com>
2017-08-25 06:42:12 +0530

    Added PDF download link

--- stats/urls.py
+++ stats/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from .views import stats_view, StatsListing, StudentStatsListing, StudentView, CurrentStudentList
+from .views import PDFview
 urlpatterns = [
   url(r'^$', stats_view),
   url(r'^search$', StatsListing.as_view()),
   url(r'^searchstudent$', StudentStatsListing.as_view()),
   url(r'^getstudent$', StudentView.as_view()),
   url(r'^currentstudent$', CurrentStudentList.as_view()),
-
+  url(r'^getpdf$', PDFview.as_view()),
 ]
