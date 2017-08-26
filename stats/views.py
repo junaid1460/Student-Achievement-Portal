b16319c7ffaf540a44b2186dcab5769917e82edb
junaid <junaid1460@gmail.com>
2017-08-26 09:05:24 +0530

    Implemeneted pdf generator.

--- stats/views.py
+++ stats/views.py
@@ -10,9 +10,13 @@ from django.http import JsonResponse
 from .serializers import DocStatsSerializer, StudentStatsSerializer
 from database.models import User
 from student.serializers import DocumentSerializer
+from django.views import View
+from django.http import Http404
 # Create your views here.
 
 def stats_view(req):
+    if not req.user.is_authenticated() or not req.user.is_staff:
+        raise Http404
     return render(req, d.stats_template, {})
 
 class StudentView(generics.ListAPIView):
@@ -32,10 +36,12 @@ class StatsListing(generics.ListAPIView):
     pagination_class = StatsPaginator
     def get_queryset(self):
         # data = json.loads(self.request.body.decode('utf-8'))
+
         try:
             data = json.loads(self.request.META['HTTP_DATA'])
         except BaseException:
             return []
+
         #indexs keys, cats, years, domains
         print(data)
         keys = data['keys']
@@ -199,3 +205,93 @@ class CurrentStudentList(generics.ListAPIView):
         
         print(self.request.GET)
         return Document.objects.all()
+
+
+class PDFview(View):
+    def get(self, request):
+      
+        # data = json.loads(self.request.body.decode('utf-8'))
+        try:
+            data = json.loads(request.META['HTTP_DATA'])
+        except BaseException:
+            return []
+        #indexs keys, cats, years, domains
+        print(data)
+        keys = data['keys']
+        cats = data['cats']
+        years = data['years']
+        domains = data['domains']
+        types = data['types']
+        subcats = set(data['subcat'] + [0])
+        sortby = data['sortby']
+        # print(data)
+        try:
+            if sortby != '':
+                tmp = Document.objects.order_by(sortby)
+            else:
+                tmp = Document.objects.all() 
+        except BaseException:
+            tmp = Document.objects.all() 
+
+        if len(domains) != 0:
+            tmp = tmp.filter(_domain__in  = domains)
+        if len(subcats) != 0:
+            tmp = tmp.filter(_sub_cat__in  = subcats)
+        if len(cats) != 0:
+            tmp = tmp.filter(_category__in  = cats)
+        if len(years) != 0:
+            tmp = tmp.filter(_year__in  = years)
+        
+        if len(types) != 0:
+            tmp = tmp.filter(_type__in  = types)
+        
+        print(tmp)
+        #keys
+        current = tmp
+        
+        
+        #keys
+        res = None
+        if len(keys) > 0:
+            for k in keys:
+                try:
+                    intk = int(k)
+                    if intk > 4610:
+                        intk = 1
+                    print(intk)
+                except BaseException:
+                    intk = 1
+                try:
+                    tmp = current.filter(
+                        Q(_user__user__username__iexact = k)|
+                        Q(_event_time__year__lte = intk,_event_time__year__gte = intk),
+                    )
+                except BaseException as e:
+
+                    print("myerror:",e)
+                    continue
+                restmp = res
+                try:
+                    if res == None:
+                        res = tmp
+                    else:
+                        if tmp.count() > 0:
+                            if res.count()> 0:
+                                res = (res | tmp).distinct()
+                            else:
+                                res = tmp
+                except BaseException as e:
+                    res = restmp
+                    print(e)
+        print(current, res)
+        if res is None:
+            tmp = User.objects.filter(id__in = [i['_user__user__id'] for i in current.filter(_verified=True).only('_user__user__id').values('_user__user__id').distinct()])
+        else:
+            tmp = User.objects.filter(id__in = [i['_user__user__id'] for i in res.filter(_verified=True).only('_user__user__id').values('_user__user__id').distinct()])
+        from weasyprint import HTML
+        import defaults
+        from django.http import HttpResponse
+        html = HTML(string= render(request,defaults.list_template,{'users':tmp}).content.decode('utf-8'))
+        pdf = html.write_pdf()
+        
+        return HttpResponse(pdf, content_type='application/pdf',)
