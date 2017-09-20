fd21937ea3babfc5544383d2b3a5782f4cd3a56a
junaid <junaid1460@gmail.com>
2017-09-20 15:22:54 +0530

    added some information about routes

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from .views import DocumentListView,delete, DocumentSetView
 urlpatterns = [
+  # listing current user documents
   url(r'^docs$', DocumentListView.as_view()),
+  # create view for upploading documents
   url(r'^docs/upload$', DocumentSetView.as_view()),
+  # delete control
   url(r'^docs/delete$', delete)
 ]
