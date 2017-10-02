12245255541e1555c0e9f683b2cb604414b42966
junaid <junaid1460@gmail.com>
2017-10-02 21:22:01 +0530

    doc for usage of init .py

--- init.py
+++ init.py
@@ -1,5 +1,7 @@
 from achievement_portal.views import new_user
+# this function creates two user for development use
 def init():
+    # this function must be called within a exception handler
     print('creating student')
     new_user('junaid1460', 'junaid1460@gmail.com', 'junaid1460')
     print('creating faculty')
