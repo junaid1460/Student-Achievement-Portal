6a3ca9f9d02bcf2012a0d2026fb2433102fa3618
junaid <junaid1460@gmail.com>
2018-01-03 18:45:09 +0530

    check if any containers are running

--- dkill.sh
+++ dkill.sh
@@ -1,1 +1,6 @@
-sudo docker ps|cut -d' ' -f1|tail -n +2|xargs -L1 sudo docker kill
+#!/bin/bash
+elms="$(docker ps|cut -d' ' -f1|tail -n +2)"
+if [ "$elms" != "" ]
+then
+    echo $elms|xargs -L1 docker kill
+fi
